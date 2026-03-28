9d0562874aec6161b526973829d9ee40e836e00e
rahulsamanta82 <rs4655742@gmail.com>
2026-03-28 19:58:03 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -69,12 +69,10 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "https://chattrix-frantend.vercel.app", // ✅ Allow all origins (adjust in production)
+  credentials: true,
+}));
 
 // ✅ Routes
 app.use("/api/auth", authRoutes);
--- src/library/utils.js
+++ src/library/utils.js
@@ -1,14 +1,21 @@
-import jwt from "jsonwebtoken"
-export const generateToken = (userId , res) =>{
-    const token = jwt.sign({userId} , process.env.JWT_SECRET,{
-        expiresIn:"7d"
-    } )
+import jwt from "jsonwebtoken";
 
-    res.cookie("jwt" , token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000, //MS
-        httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development"
-    })
-    return token;
-}
+export const generateToken = (userId, res) => {
+  const token = jwt.sign(
+    { userId },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,
+
+    // 🔥 IMPORTANT FOR PRODUCTION
+    sameSite: "None",   // allow cross-site cookies
+    secure: true,       // required when sameSite=None
+
+  });
+
+  return token;
+};
